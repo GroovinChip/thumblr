2cd6b351be5e118a1f83f380a70521bd27e80eb8
GroovinChip <groovinchip@gmail.com>
2022-04-04 17:32:52 -0400

    (thumblr): add windows runner for example

--- thumblr/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ thumblr/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import path_provider_macos
 import thumblr_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ThumblrMacosPlugin.register(with: registry.registrar(forPlugin: "ThumblrMacosPlugin"))
 }
---
+++ thumblr/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <thumblr_windows/thumblr_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ThumblrWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ThumblrWindowsPlugin"));
+}
