b2eb2032f926c87ef35f9c8d361eff21d5e0a1b6
GroovinChip <groovinchip@gmail.com>
2022-04-04 17:18:04 -0400

    (thumblr): update platform interface & fix API

--- thumblr/lib/thumblr.dart
+++ thumblr/lib/thumblr.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
-import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 import 'package:thumblr_platform_interface/thumblr_platform_interface.dart';
 
 /// Generates a thumbnail for a given video file.
-Future<Uint8List> generateThumbnail({required String filePath}) {
+Future<ui.Image> generateThumbnail({required String filePath}) {
   return ThumblrPlatform.instance.generateThumbnail(filePath: filePath);
 }
